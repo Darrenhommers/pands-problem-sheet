fe260306c3859ba4bce296ff68e203a01562436b
Darren Miller <G00425667@atu.ie>
2023-02-12 19:45:08 +0000

    .\Week 03"

---
+++ Week 03/lab3.2.1_round.py
@@ -0,0 +1,7 @@
+# Rounds a numer
+# Author: Darren Miller
+
+numberToRound= float(input("Enter a float number: "))
+roundedNumber= round(numberToRound)
+
+print ('{} rounded is {}'.format (numberToRound,roundedNumber))
---
+++ Week 03/lab3.2.3_floor.py
@@ -0,0 +1,9 @@
+# Floors a number.
+# Author: Darren Miller
+
+import math
+numberTofloor= float(input("Enter a float number: "))
+flooredNumber= math.floor(numberTofloor)
+
+print('{} floored is {}'.format (numberTofloor, flooredNumber))
+
