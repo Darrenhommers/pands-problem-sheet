716c188669f56e563ea872a42e45a28456c0b521
Darren Miller <G00425667@atu.ie>
2023-02-27 21:30:45 +0000

    .\weekday.py

--- weekday.py
+++ weekday.py
@@ -1,7 +1,8 @@
 # A program that outputs whether or not today is a weekday.
 # Author: Darren Miller
+#(https://www.pythonprogramming.in/how-to-find-current-day-is-weekday-or-weekends-in-python.html)
 
-import datetime # this function outputs what the current date/day are based on numbers 0-6.
+import datetime # this module outputs what the current date/day are based on numbers 0-6.
 
 DayNo= datetime.datetime.today().weekday()
 # Days no from 0-4 are weekdays; Days no 5 and 6 are weekend days
